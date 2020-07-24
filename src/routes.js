6d3aa44985526e3c780c4f99bec4078a19bcb043
Daniel <daniel.n10@hotmail.com>
2020-07-24 11:52:55 -0300

    estruturando a search page usando macro nunjucks

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,15 @@ const multer = require('./app/middlewares/multer')
 
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+//Home
 routes.get('/', HomeController.index)
 
+// Search
+routes.get('/products/search', SearchController.index)
+
+// Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -21,6 +27,7 @@ routes.delete('/products', ProductController.delete)
 routes.get('/ads/create', function(req, res) {
     return res.redirect("/products/create")
 })
+
 
 
 module.exports = routes
