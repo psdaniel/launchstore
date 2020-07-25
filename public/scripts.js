0f0a92eec00ec7316b26054c443c3bdb3c1eb48e
Daniel <daniel.n10@hotmail.com>
2020-07-25 16:03:00 -0300

    validação de cpf, cnpj e cep no front end

--- public/scripts.js
+++ public/scripts.js
@@ -236,5 +236,36 @@ const Validate = {
             error,
             value
         }
+    },
+    isCpfCnpj(value) {
+        let error = null
+
+        const cleanValues = value.replace(/\D/g, "")
+
+        if (cleanValues.length > 11 && cleanValues.length !== 14) {
+            error = "CNPJ incorreto"
+        }
+        else if (cleanValues.length < 12 && cleanValues.length !== 11) {
+            error = "CPF incorreto"
+        }
+        
+        return {
+            error,
+            value
+        }
+    },
+    isCep(value) {
+        let error = null
+
+        const cleanValues = value.replace(/\D/g, "")
+        
+        if (cleanValues.length !== 8) {
+            error = "CEP incorreto"
+        }
+        
+        return {
+            error,
+            value
+        }
     }
 }
