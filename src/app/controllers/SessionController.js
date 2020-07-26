48991aa06c2420c33db968b49eb7fa8e795d7d59
Daniel <daniel.n10@hotmail.com>
2020-07-26 16:54:57 -0300

    criando nova senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 
+const { hash } = require('bcryptjs')
 const crypto = require('crypto')
 const mailer = require('../../lib/mailer')
 
@@ -70,21 +71,31 @@ module.exports = {
     resetForm(req, res) {
         return res.render("session/password-reset", {token: req.query.token})
     },
-    reset(req, res) {
-        const {email, password, passwordRepeat, token} = req.body
+    async reset(req, res) {
+        const user = req.user
+
+        const {password, token } = req.body
 
         try {
-
-
             // cria um novo hash de senha
-
+            const newPassword = await hash(password, 8)
             // atualiza o usuário
+            await User.update(user.id, {
+                password: newPassword,
+                reset_token: "",
+                reset_token_expires: ""
+            })
 
             // avisa ao usuário que ele tem uma nova senha
-
+            return res.render("session/login", {
+                user: req.body,
+                success: "Senha atualizada! Faça o seu login."
+            })
         } catch(err) {
             console.error(err)
             return res.render("session/password-reset", {
+                user: req.body,
+                token,
                 error: "Erro inesperado, tente novamente!"
             })
         }
