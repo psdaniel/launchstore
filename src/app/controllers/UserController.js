4df4a80c1d1849b93fb05f6c83bf73a2dc63bef3
Daniel <daniel.n10@hotmail.com>
2020-07-26 11:40:59 -0300

    formulário de atualização do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,24 @@
 const User = require('../models/User')
+const { formatCep, formatCpfCnpj } = require('../../lib/utils')
 
 module.exports = {
     registerForm(req, res) {
-        return res.render("user/register")
+        return res.render("user/register", {user})
     },
-    show(req,res) {
-        return res.send('Ok, cadastrado!')
+    async show(req,res) {
+        const {userId: id} = req.session
+        
+        const user = await User.findOne({ where: {id} })
+        
+        if(!user) return res.render("user/register", {
+            error: "Usuário não encontrado"
+        })
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        user.cep = formatCpfCnpj(user.cep)
+
+
+        return res.render('user/index', { user })
     },
     async post(req, res) {
         
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -32,5 +32,51 @@ module.exports = {
             style: 'currency',
             currency: 'BRL'
         }).format(price/100)
+    },
+    formatCpfCnpj(value) {
+        value = value.replace(/\D/g,"")
+
+        if(value.length > 14)
+            value = value.slice(0, -1)
+
+        //check if is cnpj - 11.222.333/0001-11
+        if (value.length > 11) {
+            //11222333444455
+
+            //11.222333444455
+            value = value.replace(/(\d{2})(\d)/, "$1.$2")
+
+
+            //11.222.333444455
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+            
+            //11.222.333/444455
+            value = value.replace(/(\d{3})(\d)/, "$1/$2")
+
+            
+            //11.222.333/4444-55
+            value = value.replace(/(\d{4})(\d)/, "$1-$2")
+
+        } else {
+            //cpf 111.222.333-44
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+
+            value = value.replace(/(\d{3})(\d)/, "$1-$2")
+
+        }
+    },
+    formatCep(value) {
+        value = value.replace(/\D/g, "")
+
+        if(value.length > 8)
+            value = value.slice(0, -1)
+
+        value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+        return value
     }
 }
