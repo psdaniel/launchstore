0820088f9bc98d87615b6d36a8cd8d5b5e2070cd
Daniel <daniel.n10@hotmail.com>
2020-07-26 11:16:51 -0300

    express session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -11,6 +11,8 @@ module.exports = {
         
         
         const userId = await User.create(req.body)
+
+        req.session.userId = userId
         
         return res.redirect('/users')
     }
