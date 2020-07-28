7a3a60d274ac8c9b2f92be64f5d09e35e66a87fb
Daniel <daniel.n10@hotmail.com>
2020-07-28 16:56:01 -0300

    ajustes no serviço de carregamento de produtos

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,28 +1,18 @@
-const Product = require('../models/Product')
-const { formatPrice } = require('../../lib/utils')
+const LoadProductsService = require('../services/LoadProductService')
 
 module.exports = {
     async index(req, res) {
-        const products = await Product.findAll()
+        try {
+        
+        const allProducts = await LoadProductsService.load('products')
+        const products = allProducts
+        .filter((product, index) => index > 2 ? false : true)
 
-        if (!products) return res.send("Products not found!")
-
-        async function getImage(productId) {
-            let files = await Product.files(productId)
-            files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
-
-            return files[0]
-        }
-
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product.id)
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-            return product
-        }).filter((product, index) => index > 2 ? false : true)
-
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("home/index", { products: lastAdded })
+        return res.render("home/index", { products })
+        
+    } catch(err) {
+        console.error(err)
+    }
+    
     }
 }
--- src/app/services/LoadProductService.js
+++ src/app/services/LoadProductService.js
@@ -37,7 +37,7 @@ const LoadService = {
         this.filter = filter
         return this[service]()
     },
-    product(){
+    async product(){
         try {
             const product = await Product.findOne(this.filter)
             return format(product)
@@ -45,7 +45,7 @@ const LoadService = {
             console.error(error)
         }
     },
-    products(){
+    async products(){
         try {
             const products = await Product.findAll(this.filter)
             const productsPromise = products.map(format)
