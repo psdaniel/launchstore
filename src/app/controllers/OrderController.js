fd5e21e2f5f08d132dcb7586b521d9c4663bab94
Daniel <daniel.n10@hotmail.com>
2020-07-30 11:59:46 -0300

    criação de pedidos

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,7 +1,11 @@
 const LoadProductsService = require('../services/LoadProductService')
 const User = require('../models/User')
+criaçconst Order = require('../models/Order')
 
+
+const Cart = require('../../lib/cart')
 const mailer = require('../../lib/mailer')
+
 
 const email = (seller, product, buyer) => `
 <h2>Olá ${seller.name}</h2>
@@ -23,25 +27,55 @@ const email = (seller, product, buyer) => `
 module.exports = {
     async post(req, res) {
         try {
-        // pegar os dados do produto
-        const product = await LoadProductsService.load('product', {where: {
-            id: req.body.id
-        }})
+            //pegar os produtos do carrinho
+            const cart = Cart.init(req.session.cart)
+            
+            const buyer_id = req.session.userId
+            const filteredItems = cart.items.filter(item =>
+                item.product.user_id != buyer_id    
+            )
+            // criar o pedido
+            const createOrdersPromise = filteredItems.map(async item => {
+                let { product, price:total, quantity } = item
+                const { price, id: product_id, user_id: seller_id } = product
+                const status = "open"
 
+                const order = await Order.create({
+                    seller_id,
+                    buyer_id,
+                    product_id,
+                    price,
+                    total,
+                    quantity,
+                    status
+                })
+            // pegar os dados do produto
+            product = await LoadProductsService.load('product', {where: {
+                id: product_id
+            }})
 
-        // os dados do vendedor
-        const seller = await User.findOne({where: {id: product.user_id}})
-        // os dados do comprador
-        const buyer = await User.findOne({where: {id: req.session.userId}})
-        // enviar email com os dados da compra para o vendedor
-        await mailer.sendMail({
-            to: seller.email,
-            from: 'no-reply@launchstore.com.br',
-            subject: 'Novo pedido de compra',
-            html: email(seller, product, buyer)
-        })
-        // notificar o usuário com alguma mensagem de sucesso
-        return res.render('orders/success')
+            // os dados do vendedor
+            const seller = await User.findOne({where: {id: seller_id}})
+
+            // os dados do comprador
+            const buyer = await User.findOne({where: {id: buyer_id}})
+
+            // enviar email com os dados da compra para o vendedor
+            await mailer.sendMail({
+                to: seller.email,
+                from: 'no-reply@launchstore.com.br',
+                subject: 'Novo pedido de compra',
+                html: email(seller, product, buyer)
+            })
+
+            return order
+
+            })
+
+            await Promise.all(createOrdersPromise)
+
+            // notificar o usuário com alguma mensagem de sucesso
+            return res.render('orders/success')
 
     } catch(err) {
         // ou erro
---
+++ src/routes/orders.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = express.Router()
+
+const OrderController = require('../app/controllers/OrderController')
+
+const { onlyUsers } = require('../app/middlewares/session')
+
+
+routes.post('/', onlyUsers,OrderController.post)
+
+
+module.exports = routes
