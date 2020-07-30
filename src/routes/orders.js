4a2f0af626db4fd848142edb0347fe188cb56451
Daniel <daniel.n10@hotmail.com>
2020-07-30 14:34:25 -0300

    dados do pedido

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -7,6 +7,7 @@ const { onlyUsers } = require('../app/middlewares/session')
 
 
 routes.post('/', onlyUsers,OrderController.post)
+    .get('/', onlyUsers, OrderController.index)
 
 
 module.exports = routes
